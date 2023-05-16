e149415a80db8882723a987de83a4c56efebb5fc
Laura Salazar <laura.salazar.cien@gmail.com>
2023-05-15 22:50:43 -0400

    [UPDATE] Body structure and styles

--- digital-booking-frontend/src/components/Body/src/Body.jsx
+++ digital-booking-frontend/src/components/Body/src/Body.jsx
@@ -2,19 +2,32 @@ import classNames from "classnames";
 
 const namespace = "body";
 
-export const Body = ({ className }) => {
+const Body = ({ className }) => {
   const componentClassnames = classNames(namespace, className);
   return (
-    <main className={`${namespace}`}>
+    <main className={componentClassnames}>
       <section className={`${namespace}__search`}>
         <h3>Buscador</h3>
-        {/* Aquí iría el buscador */}</section>
+        {/* Aquí iría el buscador */}
+      </section>
       <section className={`${namespace}__categories`}>
         <h3>Categorias</h3>
-        {/* Aquí irían las categorías */}</section>
+        {/* Aquí irían las categorías */}
+      </section>
       <section className={`${namespace}__recommendations`}>
         <h3>Recomendaciones</h3>
-        {/* Aquí irían las recomendaciones de productos */}</section>
+        {/* Aquí irían las recomendaciones de productos */}
+      </section>
     </main>
   );
 };
+
+Body.propTypes = {
+  className: PropTypes.string,
+};
+
+Body.defaultProps = {
+  className: "",
+};
+
+export default Body;
