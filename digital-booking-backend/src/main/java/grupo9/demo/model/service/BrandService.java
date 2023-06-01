af3d80cf926da5a01ad730568a8e8c9abf236a76
chrisvarg <cristianvargas385@gmail.com>
2023-05-31 19:00:14 -0500

    [FIXED] new api getAllBrands

--- digital-booking-backend/src/main/java/grupo9/demo/model/service/BrandService.java
+++ digital-booking-backend/src/main/java/grupo9/demo/model/service/BrandService.java
@@ -2,12 +2,16 @@ package grupo9.demo.model.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import grupo9.demo.model.DTO.BrandDTO;
+import grupo9.demo.model.DTO.CategoryDTO;
 import grupo9.demo.model.entities.Brand;
+import grupo9.demo.model.entities.Category;
 import grupo9.demo.model.repository.IBrand;
 import grupo9.demo.model.service.InterfacesService.IBrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -53,6 +57,16 @@ public class BrandService implements IBrandService {
 
     @Override
     public Set<BrandDTO> getAll() {
-        return null;
+//        List<Category> categories = categoryRepository.findAll();
+//        Set<CategoryDTO> categoryDTO = new HashSet<>();
+//
+//        for (Category category : categories) {
+//            categoryDTO.add(mapper.convertValue(category, CategoryDTO.class));
+//        }
+        List<Brand> brands = brandRepository.findAll();
+        Set<BrandDTO> brandDTO = new HashSet<>();
+
+        brands.forEach(brand -> brandDTO.add(mapper.convertValue(brand, BrandDTO.class)));
+        return brandDTO;
     }
 }
